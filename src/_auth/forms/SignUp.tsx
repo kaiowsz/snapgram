b3215499724a33913e4f5744ee9b21cfa8a77341
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2023-11-18 08:31:57 -0300

    Layout configs and new routes

---
+++ src/_auth/forms/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const SignUp = () => {
+  return (
+    <div>
+      <Button>Clcik here</Button>
+    </div>
+  )
+}
+
+export default SignUp
