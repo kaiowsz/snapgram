157d23b2c6325e524a7b152bab3e23c1eb3b5d69
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2023-11-23 08:02:13 -0300

    Search posts

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,9 +1,29 @@
+import GridPostList from "@/components/shared/GridPostList"
+import Loader from "@/components/shared/Loader"
+import SearchResults from "@/components/shared/SearchResults"
 import { Input } from "@/components/ui/input"
+import useDebounce from "@/hooks/useDebounce"
+import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations"
 import { useState } from "react"
 
 const Explore = () => {
 
+  const { data: posts, fetchNextPage, hasNextPage } = useGetPosts()
+
   const [searchValue, setSearchValue] = useState("")
+  const debouncedValue = useDebounce(searchValue, 500)
+  const { data: searchedPosts, isFetching: isSearchFetching } = useSearchPosts(debouncedValue)
+
+  if(!posts) {
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    )
+  }
+
+  const shouldShowSearchResults = searchValue !== "";
+  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item.documents.length === 0)
 
   return (
     <div className="explore-container">
@@ -18,8 +38,21 @@ const Explore = () => {
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
         <h3 className="body-bold md:h3-bold">Popular Today</h3>
         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
-          
+          <p className="small-medium md:base-medium text-light-2">All</p>
+          <img src="/assets/filter.svg" alt="Filter" width={20} height={20} />
         </div>
+      </div>
+  
+      <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+        {shouldShowSearchResults ? (
+          <SearchResults isSearchFetching={isSearchFetching} searchedPosts={searchedPosts} />
+        ) : shouldShowPosts ? (
+          <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
+        ): (
+          posts.pages.map((item: any, index: any) => (
+            <GridPostList key={`page-${index}`} posts={item.documents} />
+          ))
+        )}
       </div>
     </div>
   )
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,5 +1,5 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getPostById, getRecentPosts, likePost, savePost, signInAccount, signOutAccount, updatePost } from "../appwrite/api"
+import { useInfiniteQuery, useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts, signInAccount, signOutAccount, updatePost } from "../appwrite/api"
 import { INewPost, INewUser, IUpdatePost } from "@/types"
 import { QUERY_KEYS } from "./queryKeys"
 
@@ -138,5 +138,29 @@ export function useDeletePost() {
                 queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
             })
         }
+    })
+}
+
+export function useGetPosts() {
+    return useInfiniteQuery({
+        queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+        queryFn: getInfinitePosts,
+        getNextPageParam: (lastPage) => {
+            if(lastPage && lastPage.documents.length === 0) {
+                return null
+            }
+
+            const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
+
+            return lastId
+        }
+    })
+}
+
+export function useSearchPosts(searchTerm: string) {
+    return useQuery({
+        queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
+        queryFn: () => searchPosts(searchTerm),
+        enabled: !!searchTerm
     })
 }
