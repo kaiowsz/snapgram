d34fd943657ebd8ff5a0f1f65cb314c0c67a91ba
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2023-12-11 19:10:38 -0300

    vercel.json config

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,7 +2,7 @@ import * as z from "zod"
 
 export const SignUpValidation = z.object({
     name: z.string().min(2, { message: "Minimum of 2 characters"}),
-    username: z.string().min(2).max(50),
+    username: z.string().min(2, "Minimum of 2 characteres").max(50),
     email: z.string().email(),
     password: z.string().min(8, { message: "Minimum of 8 characters"})
 })
